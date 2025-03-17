5d83d50f18437def6ff35e5875fd19fa24cc0fe6
LAB53 <lab53@fatec.com>
2025-03-17 09:59:15 -0300

    Projeto Finalizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string nome;
+string sobrenome;
+
+Console.Write("Insira seu Nome:");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu Sobrenome:");
+sobrenome= Console.ReadLine()!.ToUpper();
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome do catálogo: {sobrenome} {nome}");
